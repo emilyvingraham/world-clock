d3b20970a2278afc6fc056cfdab6f95107e905a4
Emily Ingraham <emilyvingraham@gmail.com>
2024-01-09 12:10:24 -0700

    resolved bug for city elements unrelated to select change event

--- javascript/index.js
+++ javascript/index.js
@@ -1,52 +1,68 @@
 setInterval(function () {
     //London Element
-    let londonDateElement = document.querySelector('#date-london');
-    
-    let londonTimeZone = moment().tz('Europe/London');
-    let londonTimeElement = document.querySelector('#time-london');
+    let londonElement = document.querySelector('#london-element');
+    if (londonElement) {
+        let londonDateElement = document.querySelector('#date-london');
 
-    londonDateElement.innerHTML = `${londonTimeZone.format(
-        'dddd MMMM Do YYYY'
-    )}`;
-    londonTimeElement.innerHTML = `${londonTimeZone.format(
-        'hh:mm:ss'
-    )} <small class="small" >${londonTimeZone.format('A')}</small>`;
+        let londonTimeZone = moment().tz('Europe/London');
+        let londonTimeElement = document.querySelector('#time-london');
+
+        londonDateElement.innerHTML = `${londonTimeZone.format(
+            'dddd MMMM Do YYYY'
+        )}`;
+        londonTimeElement.innerHTML = `${londonTimeZone.format(
+            'hh:mm:ss'
+        )} <small class="small" >${londonTimeZone.format('A')}</small>`;
+    }
 
     //Paris Element
-    let parisDateElement = document.querySelector('#date-paris');
-    let parisTimeZone = moment().tz('Europe/Paris');
-    let parisTimeElement = document.querySelector('#time-paris');
+    let parisElement = document.querySelector('#paris-element');
+    if (parisElement) {
+        let parisDateElement = document.querySelector('#date-paris');
+        let parisTimeZone = moment().tz('Europe/Paris');
+        let parisTimeElement = document.querySelector('#time-paris');
 
-    parisDateElement.innerHTML = `${parisTimeZone.format('dddd MMMM Do YYYY')}`;
-    parisTimeElement.innerHTML = `${parisTimeZone.format(
-        'hh:mm:ss'
-    )} <small class="small" >${parisTimeZone.format('A')}</small>`;
+        parisDateElement.innerHTML = `${parisTimeZone.format(
+            'dddd MMMM Do YYYY'
+        )}`;
+        parisTimeElement.innerHTML = `${parisTimeZone.format(
+            'hh:mm:ss'
+        )} <small class="small" >${parisTimeZone.format('A')}</small>`;
+    }
 
     //Tokyo Element
-    let tokyoDateElement = document.querySelector('#date-tokyo');
-    let tokyoTimeZone = moment().tz('Asia/Tokyo');
-    let tokyoTimeElement = document.querySelector('#time-tokyo');
+    let tokyoElement = document.querySelector('#tokyo-element');
+    if (tokyoElement) {
+        let tokyoDateElement = document.querySelector('#date-tokyo');
+        let tokyoTimeZone = moment().tz('Asia/Tokyo');
+        let tokyoTimeElement = document.querySelector('#time-tokyo');
 
-    tokyoDateElement.innerHTML = `${tokyoTimeZone.format('dddd MMMM Do YYYY')}`;
-    tokyoTimeElement.innerHTML = `${tokyoTimeZone.format(
-        'hh:mm:ss'
-    )} <small class="small" >${tokyoTimeZone.format('A')}</small>`;
+        tokyoDateElement.innerHTML = `${tokyoTimeZone.format(
+            'dddd MMMM Do YYYY'
+        )}`;
+        tokyoTimeElement.innerHTML = `${tokyoTimeZone.format(
+            'hh:mm:ss'
+        )} <small class="small" >${tokyoTimeZone.format('A')}</small>`;
+    }
 
     //NYC Element
-    let nycDateElement = document.querySelector('#date-nyc');
-    let nycTimeZone = moment().tz('America/New_York');
-    let nycTimeElement = document.querySelector('#time-nyc');
+    let nycElement = document.querySelector('#nyc-element');
+    if (nycElement) {
+        let nycDateElement = document.querySelector('#date-nyc');
+        let nycTimeZone = moment().tz('America/New_York');
+        let nycTimeElement = document.querySelector('#time-nyc');
 
-    nycDateElement.innerHTML = `${nycTimeZone.format('dddd MMMM Do YYYY')}`;
-    nycTimeElement.innerHTML = `${nycTimeZone.format(
-        'hh:mm:ss'
-    )} <small class="small" >${nycTimeZone.format('A')}</small>`;
+        nycDateElement.innerHTML = `${nycTimeZone.format('dddd MMMM Do YYYY')}`;
+        nycTimeElement.innerHTML = `${nycTimeZone.format(
+            'hh:mm:ss'
+        )} <small class="small" >${nycTimeZone.format('A')}</small>`;
+    }
 }, 1000);
 
 ///////////////////////////
 function updateCity(event) {
     let cityTimeZone = event.target.value;
-    let cityName = cityTimeZone.replace("_", " ").split('/')[1];
+    let cityName = cityTimeZone.replace('_', ' ').split('/')[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector('#cities');
     citiesElement.innerHTML = `<div class="city-container">
@@ -59,7 +75,9 @@ function updateCity(event) {
                         </div>
                         <div class="time-select">${cityTime.format(
                             'hh:mm:ss'
-                        )} <small class="small-select">${cityTime.format('A')}</small>
+                        )} <small class="small-select">${cityTime.format(
+        'A'
+    )}</small>
                         </div>
                     </div>
                     <div class="date">${cityTime.format('dddd MMMM Do YYYY')}
